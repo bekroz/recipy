cc663fa06c638e486815a2bd1858746f48796aa3
BEK ROZ <bekrozikoff@gmail.com>
2022-10-29 17:35:03 +0500

    Initial Commit

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by BekRoz on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
